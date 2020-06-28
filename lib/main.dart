9cdf19dfa090c34fe36faf7a99a5dbd44a92f004
Maykell Sánchez Romero <kellsaro@gmail.com>
2020-06-27 23:04:57 -0500

    Add drawler menu and basic layout in main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,43 +26,12 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -73,43 +44,158 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Kinderapp"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              height: 120.0,
+              child: DrawerHeader(
+                decoration: BoxDecoration(color: Colors.blue),
+                padding: EdgeInsets.zero,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Text(
+                      "Kinder\n  App",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    radius: 30,
+                  ),
+                  title: Text("Mamá de X"),
+                  trailing: Icon(Icons.keyboard_arrow_down),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ListTile(
+              leading: Icon(
+                FontAwesomeIcons.thLarge,
+                size: 50,
+              ),
+              title: Text("Inicio"),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.account_circle,
+                size: 50,
+              ),
+              title: Text("Mi hijo"),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.book,
+                size: 50,
+              ),
+              title: Text("Reportes"),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.mail,
+                size: 50,
+              ),
+              title: Text("Notificaciones"),
+              onTap: () => Navigator.of(context).pop(),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: SingleChildScrollView(
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+
+            children: <Widget>[
+              Container(
+                height: 75.0,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.account_circle,
+                    size: 50,
+                  ),
+                  title: Text("Mi hijo"),
+                  subtitle: Text("¿Cómo va el día de mi hijo?"),
+                  trailing: Icon(Icons.navigate_next),
+                  onTap: () => true,
+                ),
+              ),
+              Divider(
+                color: Colors.grey,
+              ),
+              Container(
+                height: 75.0,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.book,
+                    size: 50,
+                  ),
+                  title: Text("Reportes"),
+                  subtitle: Text("Ver la actuación general de mi hijo"),
+                  trailing: Icon(Icons.navigate_next),
+                  onTap: () => true,
+                ),
+              ),
+              Divider(
+                height: 15.0,
+                color: Colors.grey,
+              ),
+              Container(
+                height: 75.0,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    size: 50,
+                  ),
+                  title: Text("Notificaciones"),
+                  subtitle: Text("Mensajes importantes de la escuela"),
+                  trailing: Icon(Icons.navigate_next),
+                  onTap: () => true,
+                ),
+              ),
+              Divider(
+                height: 15.0,
+                color: Colors.grey,
+              ),
+              Container(
+                height: 75.0,
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.mapSigns,
+                    size: 50,
+                  ),
+                  title: Text("Eventos"),
+                  subtitle: Text("Actividades programadas"),
+                  trailing: Icon(Icons.navigate_next),
+                  onTap: () => true,
+                ),
+              ),
+              Divider(
+                height: 15.0,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
